f2b652adf23f91f6411796fe247692a39224ed48
cedoor <omardesogus9@gmail.com>
2018-01-04 16:47:14 +0100

    improve github service

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,17 +1,21 @@
 import {Injectable} from "@angular/core";
-import "rxjs/add/operator/map";
 import {HttpClient} from "@angular/common/http";
 
 @Injectable()
 export class GithubService {
 
-    url: string = "https://api.github.com/repos/Mindmapp/mindmapp/releases";
+    private readonly releaseUrl: string = "https://api.github.com/repos/Mindmapp/mindmapp/releases";
+    private readonly downloadLink: string = "https://github.com/Mindmapp/mindmapp/releases/download";
 
     constructor(private http: HttpClient) {
     }
 
     getLastVersion(): Promise<any> {
-        return this.http.get(this.url).toPromise().then(data => data[0].name);
+        return this.http.get(this.releaseUrl).toPromise().then(data => data[0].name);
+    }
+
+    getDownloadUrl() {
+        return this.downloadLink;
     }
 
 }
