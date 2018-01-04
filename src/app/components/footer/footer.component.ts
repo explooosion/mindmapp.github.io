541f2c49135e8f32532d09fdd7f22a9159dd58c6
cedoor <omardesogus9@gmail.com>
2018-01-04 16:46:08 +0100

    create footer component

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,25 @@
+import {Component, Input, OnInit} from "@angular/core";
+import {TranslateService} from "@ngx-translate/core";
+
+@Component({
+    selector: "app-footer",
+    templateUrl: "./footer.component.html",
+    styleUrls: ["./footer.component.scss"]
+})
+export class FooterComponent implements OnInit {
+
+    @Input() copyright: string;
+    @Input() author: string;
+    @Input() languages: string[];
+
+    constructor(public translate: TranslateService) {
+    }
+
+    ngOnInit() {
+    }
+
+    setLanguage(language: string) {
+        this.translate.use(language);
+    }
+
+}
