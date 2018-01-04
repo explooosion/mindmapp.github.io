e6b34164b6c71706f44909b919d96f1035bce850
cedoor <omardesogus9@gmail.com>
2018-01-04 16:46:54 +0100

    add angular routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,64 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
     selector: "app-root",
-    template: "<router-outlet></router-outlet>"
+    templateUrl: "app.component.html"
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-    constructor() {
+    title: string;
+
+    pages: any[];
+    socials: any[];
+
+    copyright: any;
+    author: string;
+    languages: any[];
+
+    constructor(public translate: TranslateService) {
+    }
+
+    ngOnInit() {
+        this.title = "Mindmapp";
+        this.author = "Omar Desogus";
+
+        this.pages = [];
+
+        this.socials = [{
+            name: "github",
+            link: "https://github.com/Mindmapp"
+        }];
+
+        this.copyright = {
+            name: "Copyright (c) 2017",
+            link: "https://github.com/Mindmapp/mindmapp/blob/master/LICENSE.md"
+        };
+
+        this.languages = [{
+            code: "it",
+            icon: "it"
+        }, {
+            code: "en",
+            icon: "gb"
+        }];
+
+        this.setTranslations();
+    }
+
+    setTranslations() {
+        let defaultLanguage = "en",
+            browserLanguage = this.translate.getBrowserLang();
+
+        for (let language of this.languages) {
+            if (language.code === browserLanguage) {
+                defaultLanguage = browserLanguage;
+                break;
+            }
+        }
+
+        this.translate.setDefaultLang(defaultLanguage);
+        this.translate.use(defaultLanguage);
     }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,42 @@ import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 import {AppComponent} from "./app.component";
 import {GithubService} from "./services/github.service";
-import {HttpClientModule} from "@angular/common/http";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {AppRoutingModule} from "./app-routing.module";
 import {HomeComponent} from "./pages/home/home.component";
+import {NavbarComponent} from "./components/navbar/navbar.component";
+import {FooterComponent} from "./components/footer/footer.component";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {MmpService} from "./services/mmp.service";
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
 
 @NgModule({
     declarations: [
         AppComponent,
-        HomeComponent
+        HomeComponent,
+        NavbarComponent,
+        FooterComponent
     ],
     imports: [
         BrowserModule,
         HttpClientModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        }),
         AppRoutingModule
     ],
-    providers: [GithubService],
+    providers: [
+        GithubService,
+        MmpService
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
