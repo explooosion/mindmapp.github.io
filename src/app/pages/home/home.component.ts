fdb5cf543124b172696e1dde5701eee2dc28f133
cedoor <omardesogus9@gmail.com>
2018-01-06 16:38:32 +0100

    [feat] create new components

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from "@angular/core";
 import {GithubService} from "../../services/github.service";
-import {MmpService} from "../../services/mmp.service";
 
 @Component({
     selector: "app-home",
@@ -11,48 +10,18 @@ export class HomeComponent implements OnInit {
 
     title: string = "Mindmapp";
 
-    downloadUrl: string = "https://github.com/Mindmapp/mindmapp/releases/download";
-
     version: string;
     status: string;
 
-    releases: any;
-
-    constructor(public github: GithubService,
-                public mmp: MmpService) {
+    constructor(public github: GithubService) {
     }
 
     ngOnInit() {
-        this.downloadUrl = this.github.getDownloadUrl();
-
         this.github.getLastVersion().then(version => {
             this.version = version;
         });
 
-        this.mmp.init("mmp", {
-            drag: false
-        });
-
-        this.mmp.getMap("example.mmp").then((data) => {
-            this.mmp.createMap(data);
-        });
-
         this.status = "Alpha";
-
-        this.releases = {
-            windows: {
-                os: "Windows",
-                architectures: ["32", "64"]
-            },
-            linux: {
-                os: "Linux",
-                architectures: ["32", "64"]
-            }
-        };
-    }
-
-    getPath(os: string, architecture: string) {
-        return `${this.downloadUrl}/${this.version}/mindmapp-${os.toLowerCase()}-${architecture}.zip`;
     }
 
 }
